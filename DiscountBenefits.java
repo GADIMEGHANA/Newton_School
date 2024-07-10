f4a5092d1f5d286f9a56fa0279c73064006a686f
Gadi Meghana <meghanagadi299@gmail.com>
2024-07-10 19:11:05 +0530

    Discount benefits

---
+++ DiscountBenefits.java
@@ -0,0 +1,35 @@
+package Newton_School;
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class DiscountBenefits {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        sc.close();
+        int nc=0;
+        int c=x;
+        for(int i=0;i<n;i++){
+            nc+=a[i];
+            if(a[i]>y){
+                a[i]-=y;
+                c+=a[i];
+            }
+        }
+        if(c<nc){
+            System.out.println("COUPON");
+        }
+        else{
+            System.out.println("NO COUPON");
+        }
+
+    }
+}
