b28401fb9cdd874b525b759d7b26ffc600bf1eea
Gadi Meghana <meghanagadi299@gmail.com>
2024-07-12 17:51:32 +0530

    Add all untracked files

---
+++ Integerbreak.java
@@ -0,0 +1,30 @@
+package Newton_School;
+import java.util.*;
+
+class IntegerBreak {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(integerBreak(n));
+    }
+
+    public static int integerBreak(int n) {
+        
+        if (n == 2) return 1;  
+        if (n == 3) return 2;  
+        
+       
+        int maxProduct = 1;
+        
+       
+        while (n > 4) {
+            maxProduct *= 3;
+            n -= 3;
+        }
+        
+        
+        maxProduct *= n;
+        
+        return maxProduct;
+    }
+}
