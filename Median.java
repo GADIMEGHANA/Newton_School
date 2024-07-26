9e8ba5d7a83cb32172f7e4612d2349a8e8069b5b
Gadi Meghana <meghanagadi299@gmail.com>
2024-07-26 22:10:16 +0530

    Median for free

---
+++ Median.java
@@ -0,0 +1,29 @@
+ 
+package Newton_School;
+import java.util.*;
+public class Median {
+ 
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        ArrayList<Integer> a=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            a.add(sc.nextInt());
+        }
+        sc.close();
+        Collections.sort(a);
+        int l=a.size();
+        int sum=0;
+        l=(l%2==0)?(l/2)-1:(l/2);
+        for(int i=0;i<n;i++){
+            if(i==l){
+                continue;
+            }
+            else{
+                sum+=a.get(i);
+            }
+        }
+        System.out.println(sum);
+    }
+}
