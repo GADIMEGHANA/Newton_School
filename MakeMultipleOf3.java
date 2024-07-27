51d4505ca0658b868ec6fc3cc7f39c16311e3402
Gadi Meghana <meghanagadi299@gmail.com>
2024-07-27 21:51:34 +0530

    Make Multiple Of 3

---
+++ MakeMultipleOf3.java
@@ -0,0 +1,40 @@
+package Newton_School;
+import java.util.Scanner;
+public class MakeMultipleOf3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String n = scanner.next();
+        scanner.close();
+        
+        int sum = 0;
+        int[] remainderCount = new int[3];
+        for (char digit : n.toCharArray()) {
+            int num = digit - '0';
+            sum += num;
+            remainderCount[num % 3]++;
+        }
+
+        int remainder = sum % 3;
+
+        if (remainder == 0) {
+            System.out.println(0);
+        } else {
+            if (remainderCount[remainder] > 0) {
+                if (n.length() > 1) {
+                    System.out.println(1);
+                } else {
+                    System.out.println(-1); 
+                }
+            } else if (remainderCount[3 - remainder] > 1) {
+                if (n.length() > 2) {
+                    System.out.println(2);
+                } else {
+                    System.out.println(-1); 
+                }
+                 } else {
+                System.out.println(-1);
+            }
+        }
+        scanner.close();
+    }
+}
